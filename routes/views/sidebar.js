7fd89915f525c4a930c80cf6c549ad70bbce679e
Will Diep <widiep@gmail.com>
2017-05-16 02:00:48 -0700

    implemented generated categories to sidebar categories

---
+++ routes/views/sidebar.js
@@ -0,0 +1,63 @@
+var keystone = require('keystone'),
+	async = require('async');
+
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+
+
+
+	// Init locals
+	locals.filters = {
+		category: req.params.category,
+	};
+	locals.data = {
+		posts: [],
+		categories: [],
+	};
+
+	// Load all categories
+	view.on('init', function (next) {
+
+		keystone.list('PostCategory').model.find().sort('name').exec(function (err, results) {
+
+			if (err || !results.length) {
+				return next(err);
+			}
+
+			locals.data.categories = results;
+
+			// Load the counts for each category
+			async.each(locals.data.categories, function (category, next) {
+
+				keystone.list('Post').model.count().where('categories').in([category.id]).exec(function (err, count) {
+					category.postCount = count;
+					next(err);
+				});
+
+			}, function (err) {
+				next(err);
+			});
+		});
+	});
+
+	// Load the current category filter
+	view.on('init', function (next) {
+
+		if (req.params.category) {
+			keystone.list('PostCategory').model.findOne({ key: locals.filters.category }).exec(function (err, result) {
+				locals.data.category = result;
+				next(err);
+			});
+		} else {
+			next();
+		}
+	});
+
+
+	// Render the view
+	view.render('sidebar');
+};
